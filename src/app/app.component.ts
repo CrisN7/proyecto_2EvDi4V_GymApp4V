1ed045c598a6abb95e9fca7421ffbd3c26dbc659
Cristian Rojas <cris_2110@outlook.com>
2025-01-13 19:55:08 +0100

    Componente navbar terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ProyectoFinal_2EVDi_GymApp4V';
+  title = '4V Gym';//titulo de la pestaña
 }
